317d009dcd76cc6399ae9ad6317335bb1d9168e8
jaydeaton <jayrdeaton@gmail.com>
2018-02-03 00:44:15 -0600

    updated test

--- test.js
+++ test.js
@@ -5,20 +5,25 @@ workingBar.length = 80;
 workingBar.character = '------';
 workingBar.start();
 
-// setTimeout(() => {
-//   workingBar.front = 'This is an incredibly long front: [';
-// }, 2000);
-//
-// setTimeout(() => {
-//   workingBar.character = `\x1b[36m${workingBar.character}\x1b[0m`;
-// }, 4000);
-//
-// setTimeout(() => {
-//   workingBar.front = '[';
-//   workingBar.back = '] This is an incredibly long back';
-// }, 6000);
+setTimeout(() => {
+  workingBar.front = 'This is an incredibly long front: [';
+}, 2000);
 
 setTimeout(() => {
+  workingBar.character = `\x1b[36m${workingBar.character}\x1b[0m`;
+}, 4000);
+
+setTimeout(() => {
+  workingBar.front = '[';
+  workingBar.back = '] This is an incredibly long back';
+}, 6000);
+
+setTimeout(() => {
+  workingBar.back = ']';
   workingBar.before = '\x1b[36m-';
   workingBar.character = '-\x1b[0m';
-}, 1000);
+}, 8000);
+
+setTimeout(() => {
+  workingBar.stop();
+}, 10000);
